0145fb89212fc646b53df7dbebbf82487ba3c0a4
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-12 20:21:07 -0500

    Adding Articles controller actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
