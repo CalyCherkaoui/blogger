92383425fd046f8ad6904fab81eac2155e38d0c1
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-13 15:39:57 -0500

    Adding editing and destroying functionalities to Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, warning: 'Article successfuly deleted'
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
