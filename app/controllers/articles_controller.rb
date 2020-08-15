c691808ea3f4c51ea57147a14d9034657686a94f
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-15 16:27:22 -0500

    update tag and tagging models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,6 +45,6 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :tag_list)
     end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ApplicationRecord
   validates :name, presence: true
   has_many :taggings
   has_many :articles, through: :taggings
+
+  def to_s
+    name
+  end
 end
