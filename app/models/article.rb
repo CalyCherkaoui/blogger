5bb05de59a04954ea375498bdf1f34620ea5b2b3
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-15 20:18:54 -0500

    Updating tags and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,15 +5,18 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+  # tag_list attribute setter
+
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
     self.tags = new_or_found_tags
   end
 
+  # tag_list attribute getter
   def tag_list
     self.tags.collect do | tag |
-      tag.name.join(", ")
-    end
+      tag.name
+    end.join(",")
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ApplicationRecord
   has_many :taggings
   has_many :articles, through: :taggings
 
-  def to_s
-    name
-  end
+  # def to_s
+  #   name
+  # end
 end
